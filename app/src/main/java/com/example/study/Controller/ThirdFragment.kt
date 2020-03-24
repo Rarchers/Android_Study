3054ca5f0316a7e92608457389cca3bb5c01ed42
Rarchers <2553293392@qq.com>
2020-03-24 10:33:49 +0800

    ViewPager2

--- app/src/main/java/com/example/study/Controller/ThirdFragment.kt
+++ app/src/main/java/com/example/study/Controller/ThirdFragment.kt
@@ -6,8 +6,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.study.Controller.Third.AFragment
+import com.example.study.Controller.Third.BFragment
+import com.example.study.Controller.Third.CFragment
 import com.example.study.R
 import com.example.study.Model.ThirdViewModel
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.third_fragment.*
 
 
 class ThirdFragment : Fragment() {
@@ -26,7 +32,29 @@ class ThirdFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(ThirdViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        viewpager.adapter = object : FragmentStateAdapter(this){
+            override fun getItemCount() = 3
+
+            override fun createFragment(position: Int)=
+                when(position){
+                  0 -> AFragment()
+                  1 -> BFragment()
+                  else -> CFragment()
+              }
+        }
+
+
+        TabLayoutMediator(tablayout,viewpager){
+            tab, position ->
+            when(position){
+                0 -> tab.text = "1"
+                1 -> tab.text = "2"
+                else -> tab.text = "3"
+            }
+        }.attach()
+
+
     }
 
 }
